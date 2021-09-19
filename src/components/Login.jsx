f19fff3ad59f30dfecddb495baf2f69b9d5b61c1
Aditya Rajguru <rajguru.aditya.25@gmail.com>
2021-09-19 14:24:37 +0530

    Create react app

---
+++ src/components/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { GoogleOutlined, FacebookOutlined } from "@ant-design/icons";
+
+function Login() {
+  return (
+    <div>
+      <div id="login-page"></div>
+    </div>
+  );
+}
+
+export default Login;
